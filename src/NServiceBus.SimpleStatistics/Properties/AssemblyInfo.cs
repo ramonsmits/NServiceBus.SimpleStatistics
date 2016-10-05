8f7d5f1c5915ca57fb49987ddadc6058c35461f9
Ramon Smits <ramon.smits@gmail.com>
2016-10-05 10:15:24 +0200

    GitVersion

--- src/NServiceBus.SimpleStatistics/Properties/AssemblyInfo.cs
+++ src/NServiceBus.SimpleStatistics/Properties/AssemblyInfo.cs
@@ -8,6 +8,3 @@
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
