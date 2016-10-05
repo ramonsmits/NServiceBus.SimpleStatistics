b04640189bef3e6ccd0572f7a7662a1a54b839a8
Ramon Smits <ramon.smits@gmail.com>
2016-10-05 10:29:54 +0200

    Linking to the project, not to a specific tag.

--- src/NServiceBus.SimpleStatistics/Properties/AssemblyInfo.cs
+++ src/NServiceBus.SimpleStatistics/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyTitle("NServiceBus.Statistics")]
 [assembly: AssemblyDescription("Get live statistics in your console tile or your log and a summary of the performance at endpoint shutdown.")]
-[assembly: AssemblyConfiguration("https://github.com/ramonsmits/NServiceBus.SimpleStatistics/tree/1.0.0")]
+[assembly: AssemblyConfiguration("https://github.com/ramonsmits/NServiceBus.SimpleStatistics/")]
 [assembly: AssemblyCompany("https://github.com/ramonsmits")]
 [assembly: AssemblyProduct("NServiceBus [5,6)")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
